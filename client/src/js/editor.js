92106197700d83ac19f4f9f02d18fb1220c9f843
Ilya Evlanov <evlanov.ilya@gmail.com>
2023-03-25 22:08:17 -0400

    removed console.logs

--- client/src/js/editor.js
+++ client/src/js/editor.js
@@ -26,7 +26,6 @@ export default class {
         // Fall back to localStorage if nothing is stored in indexeddb, and if neither is available, set the value to header.
         getDb().then((data) => {
             console.info('Loaded data from IndexedDB, injecting into editor')
-            console.log(data[0]?.content)
             this.editor.setValue(data[0]?.content || localData || header)
         })
 
@@ -36,7 +35,6 @@ export default class {
 
         // Save the content of the editor when the editor itself is loses focus
         this.editor.on('blur', () => {
-            console.log('The editor has lost focus')
             putDb(localStorage.getItem('content'))
         })
     }
